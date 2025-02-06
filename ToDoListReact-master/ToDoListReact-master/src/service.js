cbc491054c90f72ca0bcd125453e9f9219500992
yaelprograming <yaelprograming@gmail.com>
2025-02-06 17:12:38 +0200

    Fix react-scripts permission issue

--- ToDoListReact-master/ToDoListReact-master/src/service.js
+++ ToDoListReact-master/ToDoListReact-master/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.baseURL = "http://localhost:5282";
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.defaults.baseURL = "http://localhost:5282";
